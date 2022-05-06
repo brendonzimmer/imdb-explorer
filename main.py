1c85e5f237d0178d03628d0cb512e94952fc4391
Brendon Zimmer <bzimmer@usc.edu>
2022-05-05 19:42:09 -0700

    Refactor and make preview window, get rid of slow api calls for images

--- main.py
+++ main.py
@@ -6,152 +6,114 @@ import requests
 from tkinter import *
 from io import BytesIO
 from PIL import ImageTk, Image
-# import json
 
 API_KEY = "k_t3f76pdi"
 
-def search(title: str):
-    print("Searching...")
-    print("May take a minute. To speed up, try searching for a title with a year.")
+def detailsWindow(movie: dict, parent: Tk):
+    state = []
+    root = Tk()
+    root.title(movie['fullTitle'])
+    root.geometry("500x500")
 
-    res = requests.get(f"https://imdb-api.com/en/API/SearchMovie/k_t3f76pdi/{title}")
+    # root = Toplevel(parent)
+    # root.title(f'{movie["fullTitle"]} Details')
+    # root.geometry("800x500")
+
+    # rootFrame = Frame(root)
+    # rootFrame.place(relx=.5, rely=.5, anchor=CENTER)
+
+    # movieFrame = Frame(rootFrame)
+    # movieFrame.grid()
             
-    if res.status_code != 200:
-        print("Error:", res.status_code)
-    else:
-        # print(json.dumps(res.json(), indent=4))
-        data = res.json()['results']
-        if data is None:
-            print(f"\nError: {res.json()['errorMessage']}")
-            return
+    # root.mainloop()
 
-        ids = []
-        
-        print("\nResults:")
-        for i, movie in enumerate(data):
-            print(f"\t{i+1}. {movie['title']} {movie['description']}")
-            ids.append(movie['id'])
-
-        choice = input("\nWhich movie would you like to see? ")
-        print(f"You want to see {ids[int(choice)-1]}: \"{data[int(choice)-1]['title']}\" ")
-        print("\nComing Soon!")
-
-
-def detailsDisplay(movie: dict, thumbnail: str, images: list, movieFrame: Frame):
-    jpg = Image.open(BytesIO(requests.get(thumbnail[0:thumbnail.find("@.")] + "@._V1_UX192_CR0,3,192,264.jpg").content))
-    bytes = ImageTk.PhotoImage(jpg)
-    thumbnail = Label(movieFrame, image=bytes)
-    thumbnail.grid(row=0, column=0)
-
-    images.append(bytes)
-
-    title = Label(movieFrame, text=movie['fullTitle'])
-    title.config(font="Arial 24 bold")
-    title.grid(row=1, column=0)
-
-
-def detailsWindow(parent: Tk, movie: dict):
-    movieDetails = getMovieDetails(movie['id'])
-    if movieDetails is None:
-        print(f"Error: No movie details found for {movie['id']}.")
-        return
+def details(id: str, parent: Tk):
+    res = requests.get(f"https://imdb-api.com/en/API/Title/{API_KEY}/{id}/Images,Ratings")
     
-    images = []
+    if res.status_code != 200:
+        print("\nError:", res.status_code)
+    elif not res.json()['id']:
+        print(f"\nError: {res.json()['errorMessage']}")
+        return
+    else:
+        movie = res.json()
+        detailsWindow(movie, parent)
 
-    root = Toplevel(parent)
-    root.title(f'{movie["fullTitle"]} Details')
-    root.geometry("800x500")
+
+def previewWindow(movie: dict):
+    state = []
+    root = Tk()
+    root.title('Movie Preview')
+    root.geometry("520x600")
 
     rootFrame = Frame(root)
     rootFrame.place(relx=.5, rely=.5, anchor=CENTER)
 
-    movieFrame = Frame(rootFrame)
-    movieFrame.grid()
-            
-    detailsDisplay(movieDetails, movie['image'], images, movieFrame)
+    previewFrame = Frame(rootFrame)
+    previewFrame.grid()
+    
+    jpg = Image.open(BytesIO(requests.get(movie['image'].replace("original", "336x462")).content))
+    bytes = ImageTk.PhotoImage(jpg)
+    state.append(bytes)
+    thumbnail = Label(previewFrame, image=bytes)
+    thumbnail.grid(row=0)
+
+    try: title = Label(previewFrame, text=movie['fullTitle'])
+    except: title = Label(previewFrame, text=movie['title']+" "+movie['description'])
+    title.config(font="Arial 24 bold")
+    title.grid(row=1)
+
+    moreInfo = Button(previewFrame, text="View More", command=lambda: details(movie['id'], parent=root))
+    moreInfo.grid(row=2)
+
     root.mainloop()
 
 
-def getMovieDetails(id: str):
-    res = requests.get(f"https://imdb-api.com/en/API/Title/k_t3f76pdi/{id}/Images,Ratings")
+def preview(data: dict):
+    print()
     
-    if res.status_code != 200:
-        print("Error:", res.status_code)
-    else:
-        # print(json.dumps(res.json(), indent=4))
-        if res.json()['id'] is None:
-            print(f"\nError: {res.json()['errorMessage']}")
-            return
+    while True:
+        inp = input("View a title (enter # or 'done'): ")
         
-        return res.json()
+        if "done" in inp.lower(): break
 
-
-def moviesDisplay(data: list, images: list, moviesFrame: Frame, offset:int=0, *, root):
-    for child in moviesFrame.winfo_children(): child.destroy()
-
-    back_offset = offset - 4
-    next_offset = offset + 4
-    
-    backButton = Button(moviesFrame, text="<", command=lambda: moviesDisplay(data, images, moviesFrame, back_offset, root=root))
-    backButton.grid(row=0, column=0)
-
-    nextButton = Button(moviesFrame, text=">", command=lambda: moviesDisplay(data, images, moviesFrame, next_offset, root=root))
-    nextButton.grid(row=0, column=3)
-
-    for i, movie in enumerate(data[offset:offset+4]):
-
-        print(f"\t{movie['rank']}: {movie['fullTitle']}")
+        if inp.isdigit(): inp = int(inp)
         
-        componentFrame = Frame(moviesFrame)
-        componentFrame.grid(row=(i%4)//2, column=(i%2)+1)
+        if inp <= 0 or inp > len(data): continue
+        
+        previewWindow(data[int(inp)-1])
 
 
-        # print(movie['image'])
-        jpg = Image.open(BytesIO(requests.get(movie['image'][0:movie['image'].find("@.")] + "@._V1_UX192_CR0,3,192,264.jpg").content)) # They just changed the API while making this :( see if it changes back
-        bytes = ImageTk.PhotoImage(jpg)
-        thumbnail = Label(componentFrame, image=bytes)
-        thumbnail.grid(row=0, column=i)
-
-        images.append(bytes)
-
-        title = Label(componentFrame, text=f"{movie['rank']}: {movie['title']}")
-        title.config(font="Arial 16")
-        title.grid(row=1, column=i)
-
-        moreInfo = Button(componentFrame, text="More Info!", command=lambda: detailsWindow(root, data[offset+i]))
-        moreInfo.grid(row=2, column=i)
-
-
-def moviesWindow(data: list):
-    images = []
-
-    root = Tk()
-    root.title("Top 100 Most Popular Movies")
-    root.geometry("1000x700")
-
-    rootFrame = Frame(root)
-    rootFrame.place(relx=.5, rely=.5, anchor=CENTER)
-
-    moviesFrame = Frame(rootFrame)
-    moviesFrame.grid()
+def search(title: str):
+    print("May take a minute. To speed up, try searching for a title with a year.")
+    print("Searching...")
+    res = requests.get(f"https://imdb-api.com/en/API/SearchMovie/{API_KEY}/{title}")
             
-    moviesDisplay(data, images, moviesFrame, root=root)
-    mainloop()
+    if res.status_code != 200:
+        print("\nError:", res.status_code)
+    elif not res.json()['results']:
+        print(f"\nError: {res.json()['errorMessage']}")
+        return
+    else:
+        data = res.json()['results']
+        print("\nResults:")
+
+        for i, movie in enumerate(data): print(f"\t{i+1}. {movie['title']} {movie['description']}")
+        preview(data)
 
 
 def top100():
-    res = requests.get("https://imdb-api.com/en/API/MostPopularMovies/k_t3f76pdi")
+    res = requests.get(f"https://imdb-api.com/en/API/MostPopularMovies/{API_KEY}")
             
     if res.status_code != 200:
-        print("Error:", res.status_code)
+        print("\nError:", res.status_code)
+    elif len(res.json()['items']) == 0:
+        print(f"\nError: {res.json()['errorMessage']}")
+        return
     else:
-        # print(json.dumps(res.json(), indent=4))
         data = res.json()['items']
-        if len(data) == 0:
-            print(f"\nError: {res.json()['errorMessage']}")
-            return
-
-        moviesWindow(data)
+        for movie in data: print(f"\t{movie['rank']}: {movie['fullTitle']}")
+        preview(data)
 
 
 def menu():
@@ -171,7 +133,7 @@ def menu():
             search(title)
 
         if choice == "3":
-            print("\nGoodbye!")
+            print("\nThank you for using IMDb explorer!")
             break
 
         print("\n-------\n")
